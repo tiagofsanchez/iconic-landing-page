ab70fd06849512ba8dce9a583012c43e03e7c69b
Tiago Sanchez <tiagofsanchez@gmail.com>
2020-08-29 15:11:42 +0800

    iconin-lading-page first commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,20 @@
 import React from "react"
+import Layout from "../components/Layout/layout"
+import Hero from "../components/Hero/hero"
+import Header from "../components/Layout/header"
+import { UspImageRight, UspImageLeft } from "../components/USP/usp"
 
 export default function Home() {
-  return <div>Hello world!</div>
+  return (
+    <>
+      <Layout>
+        <Header />
+        <Hero />
+        <UspImageRight />
+        <UspImageLeft />
+        <UspImageRight />
+        <div>Hello world!</div>
+      </Layout>
+    </>
+  )
 }
